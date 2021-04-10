b98cc5da67e082706f90f4588328a5b1407fdb13
Rhutik Giradkar <rhutikgiradkar@gmail.com>
2021-04-11 00:48:39 +0530

    :sparkles: Modeling the Sign-In Form Events & State

--- lib/domain/auth/value_objects.dart
+++ lib/domain/auth/value_objects.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:notes/domain/core/failures.dart';
-import 'package:notes/domain/core/value_objects.dart';
-import 'package:notes/domain/core/value_validators.dart';
+import '../core/failures.dart';
+import '../core/value_objects.dart';
+import '../core/value_validators.dart';
 
 class EmailAddress extends ValueObject<String> {
   @override
